af22c698344341f38453dd1b210d179bf8797ce7
André Nicoletti <andre_nls@live.com>
2015-10-10 11:02:50 -0300

    Added Syntactic project back.

---
+++ LPD.Compiler.Syntactic/SyntacticException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LPD.Compiler.Syntactic
+{
+    public class SyntacticException : Exception
+    {
+        public SyntacticException()
+        {
+        }
+        
+        public SyntacticException(string message)
+            : base(message)
+        {
+        }
+
+        public SyntacticException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
