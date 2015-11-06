649ddc62e76e65f3d422f5a895cc59bea36a5e63
André Nicoletti <andre_nls@live.com>
2015-11-06 09:56:02 -0200

    Semantic.

---
+++ LPD.Compiler.Semantic/ExpressionAnalyzer.cs
@@ -0,0 +1,30 @@
+﻿using LPD.Compiler.Lexical;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPD.Compiler.Semantic
+{
+    public class ExpressionAnalyzer
+    {
+        private Stack<Token> _tokens;
+
+        public ExpressionAnalyzer()
+        {
+            _tokens = new Stack<Token>();
+        }
+
+        public void Add(Token token)
+        {
+            if (token.Symbol != Symbols.SNumero && token.Symbol == Symbols.SIdentificador)
+            {
+                for (int i = _tokens.Count - 1; i >= 0; i--)
+                {
+                    var current = _tokens.ElementAt(i);
+                }
+            }
+        }
+    }
+}
