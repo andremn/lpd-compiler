9ebc284ee055c4397726eac151e99e85cbf73bd9
André Nicoletti <andre_nls@live.com>
2015-11-17 22:50:23 -0200

    Added CodeGenerator.

---
+++ LPD.Compiler.CodeGenerator/CodeGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LPD.Compiler.CodeGenerator.Instructions;
+
+namespace LPD.Compiler.CodeGenerator
+{
+    public class CodeGenerator
+    {
+        private StringBuilder _output;
+
+        public CodeGenerator()
+        {
+            _output = new StringBuilder();
+        }
+
+        public void GenerateLabel(int labelNumber)
+        {
+            GenerateInstruction("L" + labelNumber, NULL);
+        }
+
+        public void GenerateInstruction(string instructionName)
+        {
+            GenerateInstruction(instructionName, null);
+        }
+
+        public void GenerateInstruction(string instructionName, params string[] arguments)
+        {
+            _output.Append(instructionName);
+            
+            if (arguments == null)
+            {
+                return;
+            }
+
+            int index;
+
+            for (index = 0; index < arguments.Length - 1; index++)
+            {
+                _output.Append(arguments[index]);
+                _output.Append(",");
+            }
+
+            _output.Append(arguments[index]);
+            _output.Append(Environment.NewLine);
+        }
+
+        public Task SaveToFileAsync(string path)
+        {
+            using (var file = File.Open(path, FileMode.Create))
+            {
+                using (var writer = new StreamWriter(file))
+                {
+                    return writer.WriteAsync(_output.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ LPD.Compiler.CodeGenerator/Instructions.cs
@@ -0,0 +1,145 @@
+﻿namespace LPD.Compiler.CodeGenerator
+{
+    public static class Instructions
+    {
+        /// <summary>
+        /// The START instruction name.
+        /// </summary>
+        public const string START = "START";
+
+        /// <summary>
+        /// The HLT instruction name.
+        /// </summary>
+        public const string HLT = "HLT";
+
+        /// <summary>
+        /// The LDC instruction name.
+        /// </summary>
+        public const string LDC = "LDC";
+
+        /// <summary>
+        /// The LDV instruction name.
+        /// </summary>
+        public const string LDV = "LDV";
+
+        /// <summary>
+        /// The ADD instruction name.
+        /// </summary>
+        public const string ADD = "ADD";
+
+        /// <summary>
+        /// The SUB instruction name.
+        /// </summary>
+        public const string SUB = "SUB";
+
+        /// <summary>
+        /// The MULT instruction name.
+        /// </summary>
+        public const string MULT = "MULT";
+
+        /// <summary>
+        /// The DIVI instruction name.
+        /// </summary>
+        public const string DIVI = "DIVI";
+
+        /// <summary>
+        /// The INV instruction name.
+        /// </summary>
+        public const string INV = "INV";
+
+        /// <summary>
+        /// The AND instruction name.
+        /// </summary>
+        public const string AND = "AND";
+
+        /// <summary>
+        /// The OR instruction name.
+        /// </summary>
+        public const string OR = "OR";
+
+        /// <summary>
+        /// The NEG instruction name.
+        /// </summary>
+        public const string NEG = "NEG";
+
+        /// <summary>
+        /// The CME instruction name.
+        /// </summary>
+        public const string CME = "CME";
+
+        /// <summary>
+        /// The CMA instruction name.
+        /// </summary>
+        public const string CMA = "CMA";
+
+        /// <summary>
+        /// The CMQ instruction name.
+        /// </summary>
+        public const string CEQ = "CMQ";
+
+        /// <summary>
+        /// The CDIF instruction name.
+        /// </summary>
+        public const string CDIF = "CDIF";
+
+        /// <summary>
+        /// The CMEQ instruction name.
+        /// </summary>
+        public const string CMEQ = "CMEQ";
+
+        /// <summary>
+        /// The CMAQ instruction name.
+        /// </summary>
+        public const string CMAQ = "CMAQ";
+
+        /// <summary>
+        /// The STR instruction name.
+        /// </summary>
+        public const string STR = "STR";
+
+        /// <summary>
+        /// The JMP instruction name.
+        /// </summary>
+        public const string JMP = "JMP";
+
+        /// <summary>
+        /// The JMPF instruction name.
+        /// </summary>
+        public const string JMPF = "JMPF";
+
+        /// <summary>
+        /// The NULL instruction name.
+        /// </summary>
+        public const string NULL = "NULL";
+
+        /// <summary>
+        /// The RD instruction name.
+        /// </summary>
+        public const string RD = "RD";
+
+        /// <summary>
+        /// The PRN instruction name.
+        /// </summary>
+        public const string PRN = "PRN";
+
+        /// <summary>
+        /// The ALLOC instruction name.
+        /// </summary>
+        public const string ALLOC = "ALLOC";
+
+        /// <summary>
+        /// The DALLOC instruction name.
+        /// </summary>
+        public const string DALLOC = "DALLOC";
+
+        /// <summary>
+        /// The CALL instruction name.
+        /// </summary>
+        public const string CALL = "CALL";
+
+        /// <summary>
+        /// The RETURN instruction name.
+        /// </summary>
+        public const string RETURN = "RETURN";
+    }
+}
